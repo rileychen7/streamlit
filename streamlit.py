e3775b3099a3eb504a47798686eb21d22ba63199
Riley Chen <144640158+rileychen7@users.noreply.github.com>
2023-10-30 18:40:44 -0400

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -71,11 +71,11 @@ with col1:
         <form action="https://formsubmit.co/rchen92@buffalo.edu" method="POST">
             <div style="margin-bottom: 20px;">
                 <label for="email" style="font-size: 18px;">Your Email:</label>
-                <input type="email" id="email" name="email" required style="width: 100%; padding: 10px; font-size: 16px; background-color: #c1e3c7;">
+                <input type="email" id="email" name="email" required style="width: 100%; padding: 10px; font-size: 16px; background-color: #aaadab;">
             </div>
             <div style="margin-bottom: 20px;">
                 <label for="message" style="font-size: 18px;">Your Message:</label>
-                <textarea id="message" name="message" required style="width: 100%; padding: 10px; font-size: 16px; background-color: #c1e3c7;"></textarea>
+                <textarea id="message" name="message" required style="width: 100%; padding: 10px; font-size: 16px; background-color: #aaadab;"></textarea>
             </div>
             <button type="submit" style="background-color: #2ecc71; color: #fff; padding: 10px 20px; border: none; cursor: pointer;">Send</button>
         </form>
