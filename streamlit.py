6bfd5577312dd3764340e439e35e56acab70a34f
Riley Chen <144640158+rileychen7@users.noreply.github.com>
2023-10-31 11:49:16 -0400

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -58,7 +58,7 @@ with st.container():
 st.write("---")
 st.header("Contact Me")
 
-col1, col2 = st.columns([3, 1])  
+col1, col2 = st.columns([4, 2])  
 
 email_input_width = "100%"  
 message_input_width = "100%"  
