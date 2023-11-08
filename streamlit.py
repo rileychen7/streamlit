cc08517fe870090537e6df2660609ec3a465de0a
Riley Chen <144640158+rileychen7@users.noreply.github.com>
2023-11-07 20:09:33 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -51,15 +51,13 @@ with st.container():
         Through the implementation of a user-friendly contact form and feedback system, the personal website experienced a 50% rise in user-provided insights and networking connections, 
         resulting in an enhanced online presence and increased visibility in the digital landscape.""")
 
-with image_column:
-    #st.image("", use_column_width = True)
 
-with text_column:
-    st.subheader("Campus Crumbs")
-    st.write("""This was a website my team and I created for UB Hacking which is the annual hackathon by the University at Buffalo. This was my first time going to a hackathon. 
-    Have you ever been hungry but were too tired or had no time to get food from campus, and getting Uber Eats or Doordash was just too expensive and would take too much time?
-    This is why my team and I created a platform where college students and professors can get campus food delivered directly to them! With this new application not only will students and 
-    professors get their food faster, it will also create more job opportunities for students because this will all be student-run!""")
+    with text_column:
+        st.subheader("Campus Crumbs")
+        st.write("""This was a website my team and I created for UB Hacking which is the annual hackathon by the University at Buffalo. This was my first time going to a hackathon. 
+        Have you ever been hungry but were too tired or had no time to get food from campus, and getting Uber Eats or Doordash was just too expensive and would take too much time?
+        This is why my team and I created a platform where college students and professors can get campus food delivered directly to them! With this new application not only will students and 
+        professors get their food faster, it will also create more job opportunities for students because this will all be student-run!""")
 
 
 # Contact Form
