d681ec7d6652931de241b1b0367e03e11d5f025a
Riley Chen <144640158+rileychen7@users.noreply.github.com>
2023-11-07 20:14:54 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -51,8 +51,7 @@ with st.container():
         Through the implementation of a user-friendly contact form and feedback system, the personal website experienced a 50% rise in user-provided insights and networking connections, 
         resulting in an enhanced online presence and increased visibility in the digital landscape.""")
     
-    st.write("")
-    st.write("")
+    st.markdown("<br>", unsafe_allow_html=True)
     
     with image_column:
         st.image("logo1.png", use_column_width=True)
