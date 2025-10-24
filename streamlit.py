ee5182cd4b1db489588e98e8e5a0408eb38808c1
Riley Chen <144640158+rileychen7@users.noreply.github.com>
2025-10-24 11:53:48 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,33 +2,37 @@ import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
 
-# Define the Lottie animation
+# --- Page Config ---
+st.set_page_config(page_title="My Webpage", page_icon=":snake:", layout="wide")
+
+# --- Load Lottie Animation ---
 def load_lottieur(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
-    # Header
-with st.container():
-    st.subheader("Hello, I'm Riley :wave:")
-    with st.container():
-        st.write("---")
-        left_column, right_column = st.columns(2)
-        with left_column:
-            st.header("About Me")
-            st.write("##")
-            st.write("""I am a junior at the University at Buffalo, majoring in Mathematics with a concentration in Actuarial Science, and a minor in Statistics. Outside of academics, I enjoy playing badminton, soccer, and exploring different cuisines. I’m excited to keep expanding my knowledge and embracing new challenges ahead.""")
-
-    # Display the Lottie animation
-    with right_column:
-        st_lottie(lottie_coding)
-
-st.set_page_config(page_title="My Webpage", page_icon=":snake:", layout="wide")
 
 lottie_coding = load_lottieur("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
 
-# Projects
-# --- My Projects Section ---
+# --- Header / About Me Section ---
+with st.container():
+    st.subheader("Hello, I'm Riley :wave:")
+    st.write("---")
+    left_column, right_column = st.columns(2)
+
+    with left_column:
+        st.header("About Me")
+        st.write("##")
+        st.write(
+            """I am a junior at the University at Buffalo, majoring in Mathematics with a concentration in Actuarial Science, and a minor in Statistics. 
+            Outside of academics, I enjoy playing badminton, soccer, and exploring different cuisines. 
+            I’m excited to keep expanding my knowledge and embracing new challenges ahead."""
+        )
+
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
+
+# --- Projects Section ---
 with st.container():
     st.write("---")
     st.header("My Projects")
@@ -36,35 +40,42 @@ with st.container():
 
 # --- Project 1: Personal Website ---
 with st.container():
-    image_column, text_column = st.columns((1,2))
+    image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image("streamlit_picture.png", use_container_width=True)
     with text_column:
         st.subheader("Personal Website")
-        st.write("""You're currently viewing my personal website, built with Streamlit to showcase personal projects and insights. 
-        The platform features an interactive contact form and feedback system, boosting my website's user engagement by 52%. 
-        I will be continuing to add more content in the future!""")
+        st.write(
+            """You're currently viewing my personal website, built with Streamlit to showcase personal projects and insights. 
+            The platform features an interactive contact form and feedback system, boosting my website's user engagement by 52%. 
+            I will be continuing to add more content in the future!"""
+        )
 
 # --- Project 2: Campus Crumbs ---
 with st.container():
-    image_column, text_column = st.columns((1,2))
+    image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image("campuscrumbs.png", use_container_width=True)
     with text_column:
-        st.markdown("### [Campus Crumbs](https://campuscrumbs.streamlit.app/)", unsafe_allow_html=True)
-        st.write("""There are days when the thought of leaving our dorms just to grab a meal feels like a task, especially when juggling assignments or feeling under the weather. 
-        While apps like UberEats and DoorDash exist, we, as budget-conscious college students, aim to save money and make the most of our prepaid meal plans. 
-        Order whatever's available from any dining option on campus and have your food delivered in less than 15 minutes. 
-        It's the ultimate solution for satisfying your cravings without the hassle, right at your dormsteps.""")
-        
-# Contact Form
+        st.markdown(
+            "### [Campus Crumbs](https://campuscrumbs.streamlit.app/)",
+            unsafe_allow_html=True,
+        )
+        st.write(
+            """There are days when the thought of leaving our dorms just to grab a meal feels like a task, especially when juggling assignments or feeling under the weather. 
+            While apps like UberEats and DoorDash exist, we, as budget-conscious college students, aim to save money and make the most of our prepaid meal plans. 
+            Order whatever's available from any dining option on campus and have your food delivered in less than 15 minutes. 
+            It's the ultimate solution for satisfying your cravings without the hassle, right at your dormsteps."""
+        )
+
+# --- Contact Form ---
 st.write("---")
 st.header("Contact Me")
 
-col1, col2 = st.columns([3, 1])  
+col1, col2 = st.columns([3, 1])
 
-email_input_width = "100%"  
-message_input_width = "100%"  
+email_input_width = "100%"
+message_input_width = "100%"
 
 with col1:
     st.write("Have questions or want to get in touch? Use the form below to contact me!")
@@ -74,13 +85,18 @@ with col1:
         <form action="https://formsubmit.co/rchen92@buffalo.edu" method="POST">
             <div style="margin-bottom: 20px;">
                 <label for="email" style="font-size: 18px;">Your Email:</label>
-                <input type="email" id="email" name="email" required style="width: {email_input_width}; padding: 10px; font-size: 16px; background-color: #f0f0f0; border: 1px solid #ccc; border-radius: 5px;">
+                <input type="email" id="email" name="email" required 
+                    style="width: {email_input_width}; padding: 10px; font-size: 16px; background-color: #f0f0f0; border: 1px solid #ccc; border-radius: 5px;">
             </div>
             <div style="margin-bottom: 20px;">
-                <label for "message" style="font-size: 18px;">Your Message:</label>
-                <textarea id="message" name="message" required style="width: {message_input_width}; padding: 10px; font-size: 16px; background-color: #f0f0f0; border: 1px solid #ccc; border-radius: 5px;"></textarea>
+                <label for="message" style="font-size: 18px;">Your Message:</label>
+                <textarea id="message" name="message" required 
+                    style="width: {message_input_width}; padding: 10px; font-size: 16px; background-color: #f0f0f0; border: 1px solid #ccc; border-radius: 5px;"></textarea>
             </div>
-            <button type="submit" style="background-color: #2ecc71; color: #fff; padding: 10px 20px; border: none; cursor: pointer; border-radius: 5px;">Send</button>
+            <button type="submit" 
+                style="background-color: #2ecc71; color: #fff; padding: 10px 20px; border: none; cursor: pointer; border-radius: 5px;">
+                Send
+            </button>
         </form>
         """,
         unsafe_allow_html=True,
